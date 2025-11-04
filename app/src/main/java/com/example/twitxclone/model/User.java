484a94e8546cb6387c185500c2d4cf17ab54d330
Elle Simonds <ellesimonds@gmail.com>
2025-11-04 11:16:19 -0600

    added User class and info

---
+++ app/src/main/java/com/example/twitxclone/model/User.java
@@ -0,0 +1,25 @@
+package com.example.twitxclone.model;
+
+public class User {
+
+    private String username;
+    private String dob;
+    public static final String U_KEY = "user";
+    public static final String D_KEY = "birth";
+
+    public String getUsername(){
+       return username;
+    }
+
+    public void setUsername (String username){
+        this.username = username;
+    }
+
+    public String getDob(){
+        return dob;
+    }
+
+    public void setDob(String dob){
+        this.dob = dob;
+    }
+}
