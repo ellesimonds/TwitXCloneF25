ba9109c28eba4c6d694981f769c1fef96dc27146
Elle Simonds <ellesimonds@gmail.com>
2025-11-04 12:15:02 -0600

    added SignUpActivity info

--- app/src/main/java/com/example/twitxclone/LoginActivity.java
+++ app/src/main/java/com/example/twitxclone/LoginActivity.java
@@ -5,18 +5,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.twitxclone.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.*;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button signupButton;
     Button loginButton;
 
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase database;
     View.OnClickListener loginListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -26,8 +42,46 @@ public class LoginActivity extends AppCompatActivity {
             editText = findViewById(R.id.pass_field);
             final String password = editText.getText().toString();
 
+
+            firebaseAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                    //Sign in success, update UI with the signed-in user's information
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    Intent intent = new Intent(getApplicationContext(), MessagesActivity.class);
+                        assert user != null;
+                        intent.putExtra(User.U_KEY, user.getEmail());
+
+                    //Read User info form database to retrieve dob
+                    DatabaseReference uRef = database.getReference("users");
+//                  uRef.orderByChild("email").equalTo(user.getEmail()).limitToLast(1).addListenerForSingleValueEvent(new
+//                        @Override
+//                        public void onDataChange(@NonNull DataSnapshot snapshot){
+//                            User u = snapshot.getChildren().iterator().next().getValue(User.class);
+//                            String dob = u.getDob();
+//                            intent.putExtra(User.D_KEY, dob);
+//                    }
+//                        @Override
+//                        public void onCancelled(@NonNull DatabaseError error){
+//
+//                    }
+                    ;
+
+                    startActivity(intent);
+
+                } else {
+                    //if sign in fails, display a message to the user.
+                    Toast.makeText(getApplicationContext(), "Invalid Login", Toast.LENGTH_SHORT).show();
+                }
+
+
+                }
+            });
         }
     };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +91,8 @@ public class LoginActivity extends AppCompatActivity {
         signupButton = findViewById(R.id.signup_button);
         //loginButton = findViewById(R.id.login_button);
         loginButton.setOnClickListener(loginListener);
+        firebaseAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
         //set listeners
         loginButton.setOnClickListener(loginListener);
@@ -47,7 +103,7 @@ public class LoginActivity extends AppCompatActivity {
                 Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
                 startActivity(intent);
             }
-        };
+        });
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
--- app/src/main/java/com/example/twitxclone/SignUpActivity.java
+++ app/src/main/java/com/example/twitxclone/SignUpActivity.java
@@ -1,16 +1,25 @@
 package com.example.twitxclone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.twitxclone.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 
@@ -20,8 +29,9 @@ public class SignUpActivity extends AppCompatActivity {
     EditText passwordText;
     EditText dobText;
 
+
     FirebaseDatabase database;
-    FirebaseAuth auth;
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,17 +45,40 @@ public class SignUpActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        auth = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
 
     }
 
 
     public void onSignUp(View view) {
-        String usern = usernameText.getText().toString();
+        String username = usernameText.getText().toString();
         String password = passwordText.getText().toString();
         String dob = dobText.getText().toString();
 
-
+        firebaseAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    //Sign in success, save new user in Freebase
+                    DatabaseReference databaseRef = database.getReference("users");
+                    String uid = databaseRef.push().getKey();
+                    User user = new User();
+                    user.setEmail(username);
+                    user.setDob(dob);
+                    Toast.makeText(SignUpActivity.this, uid, Toast.LENGTH_SHORT).show();
+                    databaseRef.child(uid).setValue(user);
+                    Intent intent = new Intent(SignUpActivity.this, MessagesActivity.class);
+                    intent.putExtra(User.U_KEY, user.getEmail());
+                    intent.putExtra(User.D_KEY, user.getDob());
+                    startActivity(intent);
+                } else{
+                    //if sign in fails, display a message to the user
+                    Exception e = task.getException();
+                    Log.d("EXCE", e.toString());
+                    Toast.makeText(SignUpActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 }
